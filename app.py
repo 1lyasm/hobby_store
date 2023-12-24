c6a946e38e68b96d27203601dde27664732f5ad7
1lyasm <il.mystafa@gmail.com>
2023-12-25 01:36:25 +0300

    set up project

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request, url_for, redirect
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return redirect("/login")
+
+@app.route("/home")
+def home():
+    return render_template("home.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/login", methods=('GET', 'POST'))
+def login():
+    return render_template("login.html")
